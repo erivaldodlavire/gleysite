0f203e0254220b54608c3f265e7d6cbd0ba17453
erivaldodlavire <erivaldogalvao@hotmail.com>
2026-02-07 01:14:09 +0000

    admin fix

--- js/script.js
+++ js/script.js
@@ -2,31 +2,33 @@ window.onload = function() {
     const data = JSON.parse(localStorage.getItem('siteData'));
     if (!data) return;
 
-    // 1. Básicos
+    // 1. Tema e Sobre Mim
     document.body.className = data.tema;
     if(data.sobre) document.getElementById('edit-sobre-texto').innerText = data.sobre;
-    if(data.email) document.getElementById('edit-email').innerText = data.email;
-    if(data.horario) document.getElementById('edit-horario').innerText = data.horario;
-    if(data.copy) document.getElementById('edit-copyright').innerText = data.copy;
 
-    // 2. Fotos do Espaço (Base64)
+    // 2. Fotos do Espaço (Upload Local)
     if(data.fotos.f1) document.getElementById('img-espaco-1').src = data.fotos.f1;
     if(data.fotos.f2) document.getElementById('img-espaco-2').src = data.fotos.f2;
     if(data.fotos.f3) document.getElementById('img-espaco-3').src = data.fotos.f3;
 
-    // 3. Áreas de Atuação Dinâmicas
+    // 3. Contatos e Rodapé
+    if(data.email) document.getElementById('edit-email').innerText = data.email;
+    if(data.horario) document.getElementById('edit-horario').innerText = data.horario;
+    if(data.endereco) document.getElementById('edit-endereco').innerText = data.endereco;
+    if(data.copy) document.getElementById('edit-copyright').innerText = data.copy;
+
+    // 4. Áreas de Atuação Dinâmicas
     if(data.areas && data.areas.length > 0) {
         const container = document.getElementById('container-servicos');
         container.innerHTML = data.areas.map(a => `
             <div class="card">
                 <i class="${a.icone} gold-3d"></i>
                 <h3>${a.titulo}</h3>
-                <p>Configurado via Admin</p>
             </div>
         `).join('');
     }
 
-    // 4. Redes Sociais Automáticas
+    // 5. Publicações Dinâmicas (YouTube/Instagram)
     if(data.pubs && data.pubs.length > 0) {
         const pubContainer = document.getElementById('container-publicacoes');
         pubContainer.innerHTML = data.pubs.map(p => {
